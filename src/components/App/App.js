7822407e74abe1bdca81c98a1a1d903c56d5e704
Heather Faerber <heatherdfaerber@gmail.com>
2019-12-02 19:30:45 -0700

    Refactor App into class based component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.scss';
 import '../../_variables.scss';
+import WelcomeForm from '../WelcomeForm/WelcomeForm'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          WELCOME
-        </h1>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: [],
+    }
+  }
+
+  componentDidMount() {
+    // fetch will go here I think and assign the actual array to this.state.movies
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>
+            WELCOME
+          </h1>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
