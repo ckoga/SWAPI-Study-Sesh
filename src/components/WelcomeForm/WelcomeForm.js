cbbc84b3b441097df2bd9291ec89267f1ba0a472
Heather Faerber <heatherdfaerber@gmail.com>
2019-12-02 21:13:01 -0700

    Add labels and update styling for WelcomeForm

--- src/components/WelcomeForm/WelcomeForm.js
+++ src/components/WelcomeForm/WelcomeForm.js
@@ -8,6 +8,7 @@ class WelcomeForm extends Component {
       name: '',
       quote: 'Luke. I am your father.',
       rank: '',
+      // would we need a state property for each option that would be a boolean?
       error: false,
     }
   }
@@ -15,9 +16,18 @@ class WelcomeForm extends Component {
   render() {
     return(
       <form className='form'>
+        <label>Your Name</label>
         <input type='text' name='name' value={this.state.name} />
+        <label>Your Favorite SW Quote</label>
         <input type='text' name='quote' value={this.state.quote} />
-        <input type='radio' name='rank' value={this.state.rank} />
+        <label>Your SW Knowledge  </label>
+        <label className='radios' htmlFor='rank1'><input type='radio' name='rank' value={this.state.rank} id='rank1'/>
+        Novice</label>
+        <label className='radios' htmlFor='rank2'><input type='radio' name='rank' value={this.state.rank} id='rank2'/>
+        Intermediate</label>
+        <label className='radios' htmlFor='rank3'><input type='radio' name='rank' value={this.state.rank} id='rank3'/>
+        Expert</label>
+
         <button>ENTER</button>
       </form>
     )
