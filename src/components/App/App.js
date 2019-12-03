3f3dd6dca8a0e1bd0234fcaf492510b2643edf3b
Heather Faerber <heatherdfaerber@gmail.com>
2019-12-02 19:04:40 -0700

    Style header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import './App.scss';
+import '../../_variables.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          WELCOME
+        </h1>
       </header>
     </div>
   );
