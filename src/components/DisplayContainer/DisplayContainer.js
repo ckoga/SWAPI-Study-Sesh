400002916c8cf20f641341cbcf0c5de92ea53130
Colin Turing <colin.koga@gmail.com>
2019-12-03 15:37:39 -0700

    Set up MovieCard as a functional component that will recieve props from the DisplayContainer component

--- src/components/DisplayContainer/DisplayContainer.js
+++ src/components/DisplayContainer/DisplayContainer.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './DisplayContainer.scss'
+import MovieCard from '../MovieCard/MovieCard';
 
 
-const DisplayContainer = () => {
+const DisplayContainer = ( {movies} ) => {
+  const movieCards = movies.map(movie => {
+    return <MovieCard
+      episode={movie.episode_id}
+      title={movie.title}
+      releaseDate={movie.release_date}
+      />
+  })
+  
   return (
     <main className='display-container'>
+      {movieCards}
     </main>
   )
 }
---
+++ src/components/MovieCard/MovieCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './MovieCard.scss';
+
+const MovieCard = ({ episode, title, releaseDate }) => {
+  return (
+  <article className='movie-card'>
+    <h2>Episode {episode}</h2>
+    <h3>{title}</h3>
+    <p>{releaseDate}</p>
+    <button>View Characters</button>
+  </article>
+  )
+};
+
+export default MovieCard;
