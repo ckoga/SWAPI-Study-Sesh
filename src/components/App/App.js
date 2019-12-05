42c93eb3a44ad1a5f6e91cc3a4522dadc76a8743
Colin Turing <colin.koga@gmail.com>
2019-12-05 14:36:46 -0700

    Add object creation to try and store character objects in this.state.characters

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,6 +9,7 @@ class App extends Component {
     super();
     this.state = {
       movies: [],
+      characters: [],
       error: false,
       // currentPage: 'Landing'
       // do we want/need the user object as part of the state of App
@@ -28,6 +29,7 @@ class App extends Component {
 
   componentDidMount() {
     // fetch will go here I think and assign the actual array to this.state.movies
+    
     fetch('https://swapi.co/api/films/')
       .then(response => response.json())
       .then(data => this.setState({ movies: data.results }))
@@ -43,18 +45,52 @@ class App extends Component {
         .then(charData => {
           fetch(charData.homeworld)
             .then(response => response.json())
-            .then(homeworldData => console.log('HW: ', homeworldData))
+            .then(homeworldData => {
+              // let movChar = {
+              //   name: charData.name,
+              //   homeworld: homeworldData,
+              //   hwPop: homeworld.population,
+              //   species: speciesData,
+              //   films: []
+              // }
+              console.log('HW: ', homeworldData)
+            })
           fetch(charData.species)
             .then(response => response.json())
-            .then(speciesData => console.log('species: ', speciesData))
+            .then(speciesData => {
+              // movChar = {
+              //   name: charData.name,
+              //   homeworld: homeworldData,
+              //   hwPop: homeworld.population,
+              //   species: speciesData,
+              //   films: []
+              // }
+              // return movChar;
+              console.log('species: ', speciesData)
+            })
           charData.films.map(film => {
-            fetch(film)
+            return fetch(film)
               .then(response => response.json())
-              .then(filmData => console.log('film: ', filmData))
+              .then(filmData => {
+              //   movChar.films.push(filmData)
+                console.log('film: ', filmData)
+              })
           })
           })
     })
-    console.log(promises)
+    Promise.all(promises)
+      // .then(bios => bios.map(bio => {
+      //   let movChar = {
+      //     name: charData.name,
+      //     homeworld: homeworldData,
+      //     hwPop: homeworld.population,
+      //     species: speciesData,
+      //     films: []
+      //   }
+      //   return movChar
+      // }))
+      // .then(bios => this.setState({ characters: bios }))
+    console.log(this.state.characters)
   }
   // changePage = () => {
   //   setState of currentPage
