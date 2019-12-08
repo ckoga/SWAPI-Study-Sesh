183fc58ef6825bbf368abb1d20fa0cb236056cf3
Heather Faerber <heatherdfaerber@gmail.com>
2019-12-08 10:43:29 -0700

    Add it statements for welcome form unit tests

--- src/components/WelcomeForm/WelcomeForm.test.js
+++ src/components/WelcomeForm/WelcomeForm.test.js
@@ -10,4 +10,41 @@ describe('WelcomeForm', () => {
 
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('should update state when handleChange is invoked', () => {
+
+  });
+
+  it('should update state when handleRankSelection is invoked', () => {
+    // with event param
+  });
+
+  it('should update state when handleClick is invoked with inputs completed',
+    () => {
+    // this has an if/else conditional so probably will need to test both
+  });
+
+  it('should update state when handleClick is invoke with empty input(s)',
+    () => {
+
+  });
+
+  it('should invoke handleChange when changes occur', () => {
+
+  });
+
+  it('should invoke handleRankSelection when selection changes', () => {
+
+  });
+
+  it('should invoke handleClick when button is clicked', () => {
+
+  });
+
+  it('should invoke updateUser with user object when handleClick is invoked',
+    () => {
+
+  });
+
+
 });
