e2dc58fc712c536a5e60625e76c5c2d8e06f917a
Colin Turing <colin.koga@gmail.com>
2019-12-09 15:09:19 -0700

    Add MovieCard styling to CharacterCards

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -6,7 +6,7 @@ import {Link} from 'react-router-dom';
 
 const MovieCard = ({ episode, title, releaseDate, fetchHandler }) => {
   return (
-    <article className='movie-card'>
+    <article className='card'>
       <h2>Episode {episode}</h2>
       <h3>{title}</h3>
       <p>{releaseDate}</p>
