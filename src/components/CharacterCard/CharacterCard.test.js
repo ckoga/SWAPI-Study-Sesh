aac71cabd261085e8a07d3e11232c2c8a257ff3d
Colin Turing <colin.koga@gmail.com>
2019-12-09 16:58:28 -0700

    Add propTypes to MovieCard

---
+++ src/components/CharacterCard/CharacterCard.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import CharacterCards from '../CharacterCard/CharacterCard';
+
+describe('CharacterCard', () => {
+
+  beforeEach()
+})
