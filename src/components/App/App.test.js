43f8c3855b7b483c7242d6e6157c6938d01b8304
Heather Faerber <heatherdfaerber@gmail.com>
2019-12-09 17:34:27 -0700

    Add snapshot and unit test for App

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {shallow} from 'enzyme';
 
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
+describe('App', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  })
+
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  })
+
+  it('should update state when updateUser is invoked', () => {
+    const mockUser = {
+      name: 'Robbie',
+      quote: '"Luke. I am your father."',
+      rank: 'Ranking: Padawan', };
+    const expected = {
+      name: 'Robbie',
+      quote: '"Luke. I am your father."',
+      rank: 'Ranking: Padawan', };
+    wrapper.instance().updateUser(mockUser);
+
+    expect(wrapper.state('user')).toEqual(expected);
+  })
+
+})
