921062a93b16e727698c5ae2523672472384b9bb
Heather Faerber <heatherdfaerber@gmail.com>
2019-12-10 16:18:40 -0700

    Add test for componentDidMount

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -2,15 +2,40 @@ import React from 'react';
 import App from './App';
 import {shallow} from 'enzyme';
 
+import { getFilms } from '../../apiCalls';
+jest.mock('../../apiCalls.js');
+
 describe('App', () => {
   let wrapper;
 
   beforeEach(() => {
+    getFilms.mockImplementation(() => {
+      return Promise.resolve([{
+      "title": "The Empire Strikes Back",
+      "episode_id": 5,
+      "opening_crawl": "Impressive opening starwars crawl",
+      "director": "Irvin Kershner",
+      "producer": "Gary Kurtz, Rick McCallum",
+      "release_date": "1980-05-17",
+      "characters": ["https://swapi.co/api/people/1/"],
+      "planets": ["https://swapi.co/api/planets/4/"],
+      "starships": ["https://swapi.co/api/starships/15/"],
+      "vehicles": ["https://swapi.co/api/vehicles/8/"],
+      "species": ["https://swapi.co/api/species/6/"],
+      "created": "2014-12-12T11:26:24.656000Z",
+      "edited": "2017-04-19T10:57:29.544256Z",
+      "url": "https://swapi.co/api/films/2/"
+      }])
+    })
     wrapper = shallow(<App />);
   })
 
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
+  })
+
+  it('should call getFilms after component is mounted', () => {
+    expect(getFilms).toHaveBeenCalled();
   })
 
   it('should update state when updateUser is invoked', () => {
