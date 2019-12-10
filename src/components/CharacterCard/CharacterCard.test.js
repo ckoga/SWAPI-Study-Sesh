b4924a039cc9317bf287fb3eeb994c2860658f04
Colin Turing <colin.koga@gmail.com>
2019-12-09 17:11:49 -0700

    Add snapshot testing for CharacterCard

--- src/components/CharacterCard/CharacterCard.test.js
+++ src/components/CharacterCard/CharacterCard.test.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import CharacterCards from '../CharacterCard/CharacterCard';
+import CharacterCard from '../CharacterCard/CharacterCard';
+import MovieCard from '../MovieCard/MovieCard';
 
 describe('CharacterCard', () => {
+  let characters;
+  beforeEach(() => {
+    characters = [
+      {
+        episode_id: 10000000000,
+        name: 'Heather',
+        homeworld: 'Earth',
+        species: 'Human',
+        films: ['The Best Movie']
+      }
+    ]
+  })
 
-  beforeEach()
+  it('should match the snapshot with all the data being passed in correctly', () => {
+    const wrapper = shallow(<CharacterCard 
+      key={characters[0].name}
+      name={characters[0].name}
+      homeworld={characters[0].homeworld}
+      species={characters[0].species}
+      films={characters[0].films}    
+    />)
+
+    expect(wrapper).toMatchSnapshot()
+  })
 })
