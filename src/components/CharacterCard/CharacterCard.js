a74ce88465d5975fbeb2bcfff73fcf43109690a6
Colin Turing <colin.koga@gmail.com>
2019-12-10 15:02:36 -0700

    Add commas between each movie on CharacterCards

--- src/components/CharacterCard/CharacterCard.js
+++ src/components/CharacterCard/CharacterCard.js
@@ -3,6 +3,7 @@ import '../MovieCard/MovieCard.scss';
 import './CharacterCard.scss';
 
 const CharacterCard = ({ episode_id, name, homeworld, population, species, films }) => {
+  const filmDisplay = films.join(', ')
   return (
     <div className='card character-card'>
       <h2>{name}</h2>
@@ -10,7 +11,7 @@ const CharacterCard = ({ episode_id, name, homeworld, population, species, films
         <li>Homeworld: {homeworld}</li>
         <li>Population: {population}</li>
         <li>Species: {species}</li>
-        <li>Films: {films}</li>
+        <li>Films: {filmDisplay}</li>
       </ul>
       <button className='favorite-character-btn'>favorite</button>
     </div>
